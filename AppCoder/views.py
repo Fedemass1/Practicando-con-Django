338cf6845f78340b46f79aa2fc2cbc336673c2da
Federico Ezequiel Massone <fedemassuno@gmail.com>
2023-11-25 15:08:26 -0300

    Practicando con Django

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from AppCoder.models import Curso
 
 
@@ -7,7 +7,8 @@ from AppCoder.models import Curso
 def mostrar_cursos(request):
     cursos = Curso.objects.all()  # se conecta a la BD y me trae todos los cursos (objetos)
     contexto = {
-        "cursos": cursos
+        "cursos": cursos,
+        "nombre": "Fede"
     }
     return render(request, "AppCoder/cursos.html", contexto)
 
@@ -16,14 +17,11 @@ def crear_curso(request):
     curso = Curso(nombre="Python", camada=47785)  # Cargo datos
     curso.save()  # Me guarda esta info en las tablas de la BD.
 
-    # Le paso al contexto el curso que acabo de crear
-    contexto = {"curso": curso}
-
-    return render(request, 'index.html', contexto)
+    return redirect("/app/cursos")
 
 
 def show_html(request):
-    cursos = Curso.objects.all() # Traigo la info de la BD
+    cursos = Curso.objects.all()  # Traigo la info de la BD
     curso = Curso.objects.first()
     contexto = {"curso": curso, "cursos": cursos, "nombre": "Fede"}
     return render(request, 'index.html', contexto)
